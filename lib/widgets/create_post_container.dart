2faf0873ff25228d258658b14403f4a57fc74f85
unknown <ktaher642@gmail.com>
2023-11-27 21:40:14 -0800

    Worked on create_post_container

--- lib/widgets/create_post_container.dart
+++ lib/widgets/create_post_container.dart
@@ -1,8 +1,68 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-Widget createPostContainer({user}) {
+import '../models/user_model.dart';
+
+Widget createPostContainer({User? user}) {
   return Container(
-    height: 100.0,
-    color: Colors.orange,
+    padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
+    color: Colors.white,
+    child: Column(
+      children: [
+        Row(
+          children: [
+            CircleAvatar(
+              radius: 20,
+              backgroundColor: Colors.grey[200],
+              backgroundImage: CachedNetworkImageProvider(user!.imageUrl),
+            ),
+            const SizedBox(
+              width: 8.0,
+            ),
+            Expanded(
+              child: TextFormField(
+                decoration: const InputDecoration.collapsed(
+                    hintText: 'What\'s on your mind?'),
+              ),
+            ),
+          ],
+        ),
+        const Divider(height: 10.0, thickness: 0.5),
+        SizedBox(
+          height: 40.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.videocam, color: Colors.red),
+                label: const Text(
+                  'Video',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              const VerticalDivider(width: 8.0),
+              TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.photo_library, color: Colors.green),
+                label: const Text(
+                  'Photo',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              const VerticalDivider(width: 8.0),
+              TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.video_call, color: Colors.purpleAccent),
+                label: const Text(
+                  'Room',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    ),
   );
 }
