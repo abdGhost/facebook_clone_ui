ad81451f1ec0c716c7aa59a2a2d8fc4dbfa990d2
unknown <ktaher642@gmail.com>
2023-11-28 01:20:16 -0800

    Worked on Storie Card

--- lib/widgets/profile_avatar.dart
+++ lib/widgets/profile_avatar.dart
@@ -2,13 +2,17 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:facebook_clone_ui/config/palette.dart';
 import 'package:flutter/material.dart';
 
-Widget profileAvatar({imageUrl, isActive = false}) {
+Widget profileAvatar({imageUrl, isActive = false, hasBorder = false}) {
   return Stack(
     children: [
       CircleAvatar(
         radius: 20,
-        backgroundColor: Colors.grey[200],
-        backgroundImage: CachedNetworkImageProvider(imageUrl),
+        backgroundColor: Palette.facebookBlue,
+        child: CircleAvatar(
+          radius: hasBorder ? 17.0 : 20.0,
+          backgroundColor: Colors.grey[200],
+          backgroundImage: CachedNetworkImageProvider(imageUrl),
+        ),
       ),
       isActive == true
           ? Positioned(
--- lib/widgets/stories_widget.dart
+++ lib/widgets/stories_widget.dart
@@ -1,4 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:facebook_clone_ui/config/palette.dart';
 import 'package:facebook_clone_ui/models/models.dart';
+import 'package:facebook_clone_ui/widgets/profile_avatar.dart';
 import 'package:flutter/material.dart';
 
 class Stories extends StatelessWidget {
@@ -10,9 +13,10 @@ class Stories extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 200.0,
-      color: Colors.purple,
+      color: Colors.white,
       child: ListView.builder(
           padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+          scrollDirection: Axis.horizontal,
           itemCount: stories.length + 1,
           itemBuilder: (context, index) {
             if (index == 0) {
@@ -25,7 +29,10 @@ class Stories extends StatelessWidget {
               );
             }
             final story = stories[index - 1];
-            return _StoriesCard(story: story);
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+              child: _StoriesCard(story: story),
+            );
           }),
     );
   }
@@ -38,11 +45,65 @@ class _StoriesCard extends StatelessWidget {
 
   const _StoriesCard({
     this.currentUser,
-    this.isAddStory,
+    this.isAddStory = false,
     this.story,
   });
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12.0),
+          child: CachedNetworkImage(
+            imageUrl:
+                isAddStory == true ? currentUser!.imageUrl : story!.imageUrl,
+            height: 200,
+            width: 110.0,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          height: 200,
+          width: 110,
+          decoration: BoxDecoration(
+              gradient: Palette.storyGradient,
+              borderRadius: BorderRadius.circular(12.0)),
+        ),
+        Positioned(
+          top: 8.0,
+          left: 8.0,
+          child: isAddStory == true
+              ? Container(
+                  width: 40.0,
+                  height: 40.0,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.white),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.add,
+                      color: Palette.facebookBlue,
+                    ),
+                  ),
+                )
+              : profileAvatar(
+                  imageUrl: story!.user.imageUrl, hasBorder: story!.isViewed),
+        ),
+        Positioned(
+          bottom: 8.0,
+          right: 8.0,
+          left: 8.0,
+          child: Text(
+            isAddStory == true ? 'Add to story' : story!.user.name,
+            maxLines: 2,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
