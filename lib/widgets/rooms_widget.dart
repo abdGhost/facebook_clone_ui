8e1d6d17c6e4be4ab9c018bc5750b7e8950c54b2
unknown <ktaher642@gmail.com>
2023-11-27 22:07:39 -0800

    start working on create_room_button_widget

--- lib/widgets/rooms_widget.dart
+++ lib/widgets/rooms_widget.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_clone_ui/widgets/create_room_button.dart';
 import 'package:flutter/material.dart';
 
 Widget rooms({onLinUser}) {
@@ -5,9 +6,13 @@ Widget rooms({onLinUser}) {
     color: Colors.orange,
     height: 60.0,
     child: ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 4.0),
       scrollDirection: Axis.horizontal,
       itemCount: onLinUser.length + 1,
       itemBuilder: (context, index) {
+        if (index == 0) {
+          return createRoomButton();
+        }
         return Container(
           margin: const EdgeInsets.all(2.0),
           width: 20,
